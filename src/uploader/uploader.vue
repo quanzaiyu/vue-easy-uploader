43d8e527f2eb1e69661da0c8fdb3d9dc71793ca9
quanzaiyu <731734107@qq.com>
2017-12-22 10:04:51 +0800

    mod

--- src/uploader/uploader.vue
+++ src/uploader/uploader.vue
@@ -7,14 +7,15 @@
       >
         <img :src="file.src" alt="" ondragstart="return false;">
         <span class="file-remove" @click="remove(index)">+</span>
+        <span class="flie-loading" v-if="imgStatus==='uploading'">上传中...</span>
+        <span class="flie-finished" v-if="imgStatus==='finished'">已上传</span>
       </section>
-      <section class="file-item" v-if="imgStatus !== 'finished'">
+      <section class="file-item" v-if="imgStatus !== 'finished' && imgStore.length < 5">
         <div class="add">
-          <span>+</span>
-          <input type="file" pictype='30010003' multiple
-            data-role="none" accept="image/*"
-            @change="selectImgs"
-            ref="file"
+          <span><i class="fa fa-camera"></i></span>
+          <!-- accept="image/jpeg,image/png" capture="camera" -->
+          <input type="file" multiple accept="image/*"
+            @change="selectImgs" ref="file"
           >
         </div>
       </section>
@@ -37,12 +38,13 @@ export default {
   data () {
     return {
       files: [], // 文件缓存
-      index: 0 // 序列号
+      index: 0, // 序列号
+      maxLength: 5 // 图片最大数量
     }
   },
   computed: {
     ...mapState({
-      imgStore: state => state.imgstore.img_upload_cache,
+      imgStore: state => state.imgstore.img_upload_cache.slice(0, 5),
       imgPaths: state => state.imgstore.img_paths,
       imgStatus: state => state.imgstore.img_status
     })
@@ -51,26 +53,24 @@ export default {
     // 选择图片
     selectImgs () {
       let fileList = this.$refs.file.files
-      for (let i = 0; i < fileList.length; i++) {
-        // 文件过滤
-        if (fileList[i].name.match(/.jpg|.gif|.png|.bmp/i)) {
-          let item = {
-            key: this.index++,
-            name: fileList[i].name,
-            size: fileList[i].size,
-            file: fileList[i]
-          }
-          // 将图片文件转成BASE64格式
-          let reader = new FileReader()
-          reader.onload = (e) => {
-            this.$set(item, 'src', e.target.result)
-          }
-          reader.readAsDataURL(fileList[i])
-          this.files.push(item)
-          this.$store.commit('set_img_upload_cache', this.files) // 存储文件缓存
-          this.$store.commit('set_img_status', 'selected') // 更新文件上传状态
+      for (let i = 0, len = fileList.length; i < len; i++) {
+        let item = {
+          key: this.index++,
+          name: fileList[i].name,
+          size: fileList[i].size,
+          file: fileList[i]
         }
+        // 将图片文件转成BASE64格式
+        let reader = new FileReader()
+        reader.onload = (e) => {
+          this.$set(item, 'src', e.target.result)
+        }
+        reader.readAsDataURL(fileList[i])
+        this.files.push(item)
       }
+      this.files.splice(5)
+      this.$store.commit('set_img_upload_cache', this.files.slice(0, 5)) // 存储文件缓存
+      this.$store.commit('set_img_status', 'selected') // 更新文件上传状态
     },
     // 上传图片
     submit () {
@@ -79,7 +79,6 @@ export default {
         item.name = 'imgFiles[' + index + ']'
         formData.append(item.name, item.file)
       })
-      formData.forEach((v, k) => console.log(k, ' => ', v))
       // 新建请求
       const xhr = new XMLHttpRequest()
       xhr.open('POST', this.url, true)
@@ -89,16 +88,16 @@ export default {
           let datas = JSON.parse(xhr.responseText)
           console.log('response: ', datas)
           // 存储返回的地址
-          let imgUrlPaths = new Set()
+          let imgUrlPaths = []
           datas.forEach(e => { // error === 0为成功状态
-            e.error === 0 && imgUrlPaths.add(e.url)
+            e.error === 0 && imgUrlPaths.push(e.url)
           })
           this.$store.commit('set_img_paths', imgUrlPaths) // 存储返回的地址
           this.files = [] // 清空文件缓存
           this.index = 0 // 初始化序列号
           this.$store.commit('set_img_status', 'finished') // 更新文件上传状态
         } else {
-          alert(`${xhr.status} 请求错误!`)
+          this.$toast(`${xhr.status} 请求错误!`)
         }
       }
     },
@@ -137,10 +136,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
-@main_color: #01B5CE;
-@sub_color: #999;
-@bd_color: #F2F2F2;
-
+@import '../../styles/base.less';
 .imgUploader  {
   margin: 1em 0 2em;
   border: 1px solid #e5e5e5;
@@ -168,7 +164,6 @@ export default {
       .file-remove {
         position: absolute;
         right: 12px;
-        display: none;
         top: 4px;
         width: 14px;
         height: 14px;
@@ -178,6 +173,22 @@ export default {
         border-radius: 100%;
         transform: rotate(45deg);
         background: rgba(0, 0, 0, 0.5);
+      }
+      .flie-loading {
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 1em;
+        color: #fff;
+        background-color: rgba(0,0,0,.5);
+      }
+      .flie-finished {
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 1em;
+        color: #fff;
+        background-color: rgba(0,0,0,.5);
       }
       &:hover .file-remove {
         display: inline;
@@ -206,7 +217,12 @@ export default {
     cursor: pointer;
     border: 1px dashed @bd_color;
     color: @sub_color;
-    position: relative;
+    position: relative;  
+    background: @bd_color;
+    .fa {
+      font-size: 1.4em;
+      color: #7DD2D9;
+    }
   }
   .uploadBtn {
     position: relative;
@@ -221,12 +237,12 @@ export default {
   }
 }
 input[type="file"] {
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 80px;
-    height: 80px;
-    border: 1px solid #000;
-    opacity: 0;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 80px;
+  height: 80px;
+  border: 1px solid #000;
+  opacity: 0;
 }
 </style>
